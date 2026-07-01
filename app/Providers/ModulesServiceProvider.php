95668b7c51673e603992df199c3e31a007b2af91
Ricopie <116867325+ricopie@users.noreply.github.com>
2026-07-02 01:39:26 +0700

    Update ModulesServiceProvider.php
    add logger to handle if module and service provider is not found

--- app/Providers/ModulesServiceProvider.php
+++ app/Providers/ModulesServiceProvider.php
@@ -17,9 +17,11 @@ class ModulesServiceProvider extends ServiceProvider
         foreach ($enableModules as $module) {
             $providerClass = "Modules\\{$module}\\Providers\\{$module}ServiceProvider";
 
-            if (class_exists($providerClass)) {
-                $this->app->register($providerClass);
+             if (! class_exists($providerClass)) {
+            	logger()->warning("Module service provider not found.", ['module' => $module, 'provider' => $providerClass,]);
+                continue;
             }
+             $this->app->register($providerClass);
         }
     }
 
