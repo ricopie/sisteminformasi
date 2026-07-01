37cf29b461a2aa31bbe93ea7d51e9b5d096d7245
Enrico Pradana <myiico.sorapps@hotmail.com>
2026-07-02 00:53:07 +0700

    feat(module): add service provider with database/migrations

---
+++ modules/Beneficiary/Providers/BeneficiaryServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Modules\Beneficiary\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class BeneficiaryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
+    }
+}
