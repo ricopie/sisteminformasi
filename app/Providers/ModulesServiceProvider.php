c34ee3a76fe8d518ec82b574005e0fcdb7ccf69e
Enrico Pradana <myiico.sorapps@hotmail.com>
2026-07-01 16:41:32 +0700

    feat(module): add module feature with enable modules configuration

---
+++ app/Providers/ModulesServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModulesServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        // Register Service Provider with module enabled configuration
+        $enableModules = config('modules.enabled', []);
+
+        foreach ($enableModules as $module) {
+            $providerClass = "Modules\\{$module}\\Providers\\{$module}ServiceProvider";
+
+            if (class_exists($providerClass)) {
+                $this->app->register($providerClass);
+            }
+        }
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
