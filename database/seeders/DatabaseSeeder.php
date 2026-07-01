f86fc8fef0387b049d825f0353eefafe00732b72
Enrico Pradana <myiico.sorapps@hotmail.com>
2026-07-02 00:34:46 +0700

    chore: delete factory for FamilyCard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\FamilyCard;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -14,18 +13,5 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
-
-        FamilyCard::factory()
-            ->count(5)
-            ->has(
-                \App\Models\FosterChild::factory()
-                    ->count(3)
-                    ->has(\App\Models\ChildEducation::factory(), 'childEducation')
-                    ->has(\App\Models\EducationHistory::factory()->count(2), 'educationHistories')
-                    ->has(\App\Models\AcademicRecord::factory()->count(4), 'academicRecords')
-                    ->has(\App\Models\EducationFunding::factory(), 'educationFundings'),
-                'familyMember'
-            )
-            ->create();
     }
 }
