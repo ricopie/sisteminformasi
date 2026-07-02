f9445af83923924e7c8eb3925aa615dbde52510a
Enrico Pradana <myiico.sorapps@hotmail.com>
2026-07-02 08:08:23 +0700

    style: code style changes

--- app/Providers/ModulesServiceProvider.php
+++ app/Providers/ModulesServiceProvider.php
@@ -17,11 +17,12 @@ class ModulesServiceProvider extends ServiceProvider
         foreach ($enableModules as $module) {
             $providerClass = "Modules\\{$module}\\Providers\\{$module}ServiceProvider";
 
-             if (! class_exists($providerClass)) {
-            	logger()->warning("Module service provider not found.", ['module' => $module, 'provider' => $providerClass,]);
+            if (! class_exists($providerClass)) {
+                logger()->warning('Module service provider not found.', ['module' => $module, 'provider' => $providerClass]);
+
                 continue;
             }
-             $this->app->register($providerClass);
+            $this->app->register($providerClass);
         }
     }
 
