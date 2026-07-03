6d8e28321233b366852b413871fb34e262d6ee98
Enrico Pradana <myiico.sorapps@hotmail.com>
2026-07-03 21:23:51 +0700

    feat: implement ULID-based BeneficiaryId

---
+++ modules/Beneficiary/ValueObjects/BeneficiaryId.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Modules\Beneficiary\ValueObjects;
+
+use App\ValueObjects\Identifier;
+use Illuminate\Support\Str;
+
+final readonly class BeneficiaryId extends Identifier
+{
+    public static function generate(): static
+    {
+        return new self((string) Str::ulid());
+    }
+
+    protected static function isValid(string $value): bool
+    {
+        return Str::isUuid($value);
+    }
+}
